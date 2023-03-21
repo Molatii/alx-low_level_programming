44ca1cacc1ac596bc2efb5840a45add7bc9af6c6
Molatii <molatipaballo@gmail.com>
2023-03-21 15:16:23 -0700

    fix function name to jack_bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * time_table -  every minute of the day
+ * jack_bauer -  every minute of the day
  * Description: ' prints time starting from 00:00 to 23:59'
  * Return: 0
  */
-void time_table(void)
+void jack_bauer(void)
 {
 	int a, b, c, d;
 
